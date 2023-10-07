575ca6fbee1ed1829a131bfce2b1a21eefdba170
Vladislav Mai <vladislav.maitsvetov@indriver.com>
2023-10-07 17:19:02 +0300

    added wait pid

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!bin/bash
 
+session="daily_counter"
 pid=$(ps | grep '\./main' | grep -v 'grep' | awk '{print $1}')
 echo "pid=$pid"
+
 if [[ $pid -ne '' ]]
 then
     kill -2 $pid
+    wait $pid
+    echo "stopped pid=$pid"
+    tmux kill-session -t $session
+    echo "killed session $session"
+else
+    echo "not found already running app"
 fi
 
-tmux new-session -d -s daily_counter 'go build -o main main.go processor.go && ./main'
+tmux new-session -d -s $session 'go build -o main main.go processor.go && ./main'
