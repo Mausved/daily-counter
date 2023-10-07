403d39be3f2c870bb0ae75f6d856434536146210
Vladislav Mai <vladislav.maitsvetov@indriver.com>
2023-10-07 18:55:14 +0300

    send func, upd ci

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,27 @@
 #!bin/bash
 
 session="daily_counter"
-pid=$(ps -e | grep $session | grep -v 'grep' | awk '{print $1}')
-echo "pid=$pid"
+env GOOS=linux GOARCH=amd64 go build -o $session main.go processor.go
+rsync -avz ./$session  mausved@62.84.117.24:/home/mausved/dailycounter
 
-if [[ $pid -ne '' ]]
-then
-    kill -2 $pid
-    wait $pid
-    echo "stopped pid=$pid"
-    tmux kill-session -t $session
-    echo "killed session $session"
-else
-    echo "not found already running app"
-fi
+restart() {
+    session_name="daily_counter"
+    pid=$(ps -e | grep ${session} | grep -v 'grep' | awk '{print $1}')
+    echo "pid=$pid"
 
-tmux new-session -d -s $session "go build -o $session main.go processor.go && ./$session"
+    if [[ $pid -ne '' ]]
+    then
+        kill -2 $pid
+        wait $pid
+        echo "stopped pid=$pid"
+        tmux kill-session -t $session_name
+        echo "killed session $session_name"
+    else
+        echo "not found already running app"
+    fi
+    tmux new-session -d -s $session_name "cd dailycounter && ./daily_counter"
+    echo "started app"
+}
+
+ssh mausved@62.84.117.24 "$(typeset -f); restart"
+rm $session
