34e43e53d3d6ac1d416fbd8e0980d78d554359b7
Vladislav Maitsvetov <142788110+limassolius@users.noreply.github.com>
2023-10-08 13:23:41 +0300

    Update ci.yml (#18)
    Update actions

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,27 @@
 #!bin/bash
 
-pid=$(ps | grep '\./main' | grep -v 'grep' | awk '{print $1}')
-if [[ $pid -ne '' ]]
-then
-    kill -2 $pid
-fi
+session="daily_counter"
+env GOOS=linux GOARCH=amd64 go build -o $session main.go processor.go
+rsync -avz ./$session  $SSH_USER@$SSH_HOST:$SSH_PATH
 
-tmux new-session -d -s daily_counter 'go build -o main main.go processor.go && ./main'
+restart() {
+    session="daily_counter"
+    pid=$(ps -e | grep ${session} | grep -v 'grep' | awk '{print $1}')
+    echo "pid=$pid"
+
+    if [[ $pid -ne '' ]]
+    then
+        kill -2 $pid
+        wait $pid
+        echo "stopped pid=$pid"
+        tmux kill-session -t $session
+        echo "killed session $session"
+    else
+        echo "not found already running app"
+    fi
+    tmux new-session -d -s $session "cd dailycounter && ./$session"
+    echo "started app"
+}
+
+ssh $SSH_USER@$SSH_HOST "$(typeset -f); restart"
+rm $session
